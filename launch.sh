4120f65dc097ae9311d9903cf88d15c8a26a6ba3
Gaël PORTAY <gael.portay@rtone.fr>
2025-03-27 18:10:39 +0100

    Remove gazebo-11's models from GAZEBO_RESOURCE_PATH
    This removes the gazebo-11's models system directory from the environ
    GAZEBO_RESOURCE_PATH that appears to be unnecessary.

--- launch.sh
+++ launch.sh
@@ -3,5 +3,5 @@
 set -e
 
 source install/setup.bash
-export GAZEBO_RESOURCE_PATH="$PWD/install/robot_description/share/robot_description:/usr/share/gazebo-11/models:/usr/share/gazebo-11"
+export GAZEBO_RESOURCE_PATH="$PWD/install/robot_description/share/robot_description:/usr/share/gazebo-11"
 exec ros2 launch robot_control visual_sim.launch.py
