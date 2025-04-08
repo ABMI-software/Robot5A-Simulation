efb33451a7426346eb66bd01f7645b0e82e4ec42
linux_os <matthieuc-g@hotmail.fr>
2025-04-08 15:20:33 +0200

    add : added new launch

--- src/robot_data_process/setup.py
+++ src/robot_data_process/setup.py
@@ -10,6 +10,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/joint_sync_moveit.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
